9d003b3acc7565398710aa1bd7ea206a6151840f
Shimpo-Yumiko <s2022056@stu.musashino-u.ac.jp>
2022-08-02 01:28:29 +0900

    test indent

--- MyApp/templates/header.php
+++ MyApp/templates/header.php
@@ -1,50 +1,50 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css?family=Alice&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link href="../static/css/login.css" rel="stylesheet">
-    <link href="../static/css/calendar.css" rel="stylesheet">
-    <link href="../static/css/map.css" rel="stylesheet">
-    <link href="../static/css/add.css" rel="stylesheet">
-    <link href="../static/css/create.css" rel="stylesheet">
-    <link href="../static/css/header.css" rel="stylesheet">
-    <link href="../static/css/wishlists.css" rel="stylesheet">
-    <link href="../static/css/register.css" rel="stylesheet">
-    <title>Document</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Alice&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link href="../static/css/login.css" rel="stylesheet">
+        <link href="../static/css/calendar.css" rel="stylesheet">
+        <link href="../static/css/map.css" rel="stylesheet">
+        <link href="../static/css/add.css" rel="stylesheet">
+        <link href="../static/css/create.css" rel="stylesheet">
+        <link href="../static/css/header.css" rel="stylesheet">
+        <link href="../static/css/wishlists.css" rel="stylesheet">
+        <link href="../static/css/register.css" rel="stylesheet">
+        <title>Document</title>
+    </head>
 
 
-<body>
-    <header>
-        <div class="nav-img">
-            <nav class="navbar navbar-expand navbar-light fixed-top" >
-                <a class="navbar-brand nav-title" href="../views/home.php">Diary</a>
+    <body>
+        <header>
+            <div class="nav-img">
+                <nav class="navbar navbar-expand navbar-light fixed-top" >
+                    <a class="navbar-brand nav-title" href="../views/home.php">Diary</a>
 
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link nav-col" href="../views/home.php">Memory</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link nav-col" href="../views/wishlists.php">Wishlist</a>
-                        </li>
-                        <li class="nav-item">
-                            <form action="../templates/header.php" method="POST">
-                                <input type="submit" name="pop" value="Log Out">
-                            </form>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </div>
-    </header>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    
+                    <div class="collapse navbar-collapse" id="navbarCollapse">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link nav-col" href="../views/home.php">Memory</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link nav-col" href="../views/wishlists.php">Wishlist</a>
+                            </li>
+                            <li class="nav-item">
+                                <form action="../templates/header.php" method="POST">
+                                    <input type="submit" name="pop" value="Log Out">
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </header>
 
 
 
--- MyApp/views/home.php
+++ MyApp/views/home.php
@@ -22,53 +22,53 @@ if (!isset($_SESSION["user_id"])) {
 <?php include("../templates/header.php"); ?>
 <?php echo $user_id; ?>
 
-<!-- here is content -->
-<div class="v2_21 col-md-10">
-    <!-- カレンダー -->
-    <!-- <div class="v2_85 mr-md-3 pt-3 px-3 pt-md-5 px-md-5 overflow-hidden"> -->
-    <div class="v2_85 col-md-6">
-        <!-- <div class="v2_83"></div>
-        <div class="v2_84"></div> -->
-        <div class="v2_38"></div>
-        <div class="container-calendar">
-            <h4 id="monthAndYear"></h4>
-            <div class="button-container-calendar">
-                <button id="previous" onclick="previous()">‹</button>
-                <button id="next" onclick="next()">›</button>
-            </div>
-            
-            <table class="table-calendar" id="calendar" data-lang="ja">
-                <thead id="thead-month"></thead>
-                <tbody id="calendar-body"></tbody>
-            </table>
-            <br>
-            <div class="footer-container-calendar">
-                <label for="month"></label>
-                <select id="month" onchange="jump()">
-                    <option value=0>January</option>
-                    <option value=1>February</option>
-                    <option value=2>March</option>
-                    <option value=3>April</option>
-                    <option value=4>May</option>
-                    <option value=5>June</option>
-                    <option value=6>July</option>
-                    <option value=7>August</option>
-                    <option value=8>September</option>
-                    <option value=9>October</option>
-                    <option value=10>November</option>
-                    <option value=11>December</option>
-                </select>
-                <select id="year" onchange="jump()"></select>
-            </div>
-        </div>
-        <div class="under_line"></div>
-        <!-- mapへ -->
-        <div class="v8_367"></div>
-    </div>
+            <!-- here is content -->
+            <div class="v2_21 col-md-10">
+                <!-- カレンダー -->
+                <!-- <div class="v2_85 mr-md-3 pt-3 px-3 pt-md-5 px-md-5 overflow-hidden"> -->
+                <div class="v2_85 col-md-6">
+                    <!-- <div class="v2_83"></div>
+                    <div class="v2_84"></div> -->
+                    <div class="v2_38"></div>
+                    <div class="container-calendar">
+                        <h4 id="monthAndYear"></h4>
+                        <div class="button-container-calendar">
+                            <button id="previous" onclick="previous()">‹</button>
+                            <button id="next" onclick="next()">›</button>
+                        </div>
+                        
+                        <table class="table-calendar" id="calendar" data-lang="ja">
+                            <thead id="thead-month"></thead>
+                            <tbody id="calendar-body"></tbody>
+                        </table>
+                        <br>
+                        <div class="footer-container-calendar">
+                            <label for="month"></label>
+                            <select id="month" onchange="jump()">
+                                <option value=0>January</option>
+                                <option value=1>February</option>
+                                <option value=2>March</option>
+                                <option value=3>April</option>
+                                <option value=4>May</option>
+                                <option value=5>June</option>
+                                <option value=6>July</option>
+                                <option value=7>August</option>
+                                <option value=8>September</option>
+                                <option value=9>October</option>
+                                <option value=10>November</option>
+                                <option value=11>December</option>
+                            </select>
+                            <select id="year" onchange="jump()"></select>
+                        </div>
+                    </div>
+                    <div class="under_line"></div>
+                    <!-- mapへ -->
+                    <div class="v8_367"></div>
+                </div>
 
-    <!-- スクロール -->
-    <div class="col-md-6">
-        <div class="memorys">
+                <!-- スクロール -->
+                <div class="col-md-6">
+                    <div class="memorys">
 <?php
 
 $diary_row = selectdata("diary",$user_id);
@@ -99,84 +99,84 @@ for ($i = 0; $i < pg_num_rows($diary_row); $i++){
 
 
 ?>
-            <div class="memory" style="top: 0px;">
-                <div class="frame-select"></div>
-                <p class="memory-date">Jun.10, 2022</p>
-                <p class="memory-loc">新大久保</p>
-                <p class="memory-cap">辛いものいっぱいかと思ったけどそうではなさそう…？</p>
+                <div class="memory" style="top: 0px;">
+                    <div class="frame-select"></div>
+                    <p class="memory-date">Jun.10, 2022</p>
+                    <p class="memory-loc">新大久保</p>
+                    <p class="memory-cap">辛いものいっぱいかと思ったけどそうではなさそう…？</p>
+                </div>
+                <div class="memory" style="top: 15px;">
+                    <div class="frame-normal"></div>
+                    <p class="memory-date">Jun.04, 2022</p>
+                    <p class="memory-loc">越谷レイクタウン</p>
+                    <p class="memory-cap">VSパークに遊びに行った〜</p>
+                </div>
+                <div class="memory" style="top: 30px;">
+                    <div class="frame-normal"></div>
+                    <p class="memory-date">Jun.04, 2022</p>
+                    <p class="memory-loc">越谷レイクタウン</p>
+                    <p class="memory-cap">卵と私に初潜入！美味しかった〜！</p>
+                </div>
+                <div class="memory" style="top: 45px;">
+                    <div class="frame-normal"></div>
+                    <p class="memory-date">May.30, 2022</p>
+                    <p class="memory-loc">南大沢</p>
+                    <p class="memory-cap">やることやって南大沢へ！</p>
+                </div>
+                <div class="memory" style="top: 60px;">
+                    <div class="frame-normal"></div>
+                    <p class="memory-date">Jun.10, 2022</p>
+                    <p class="memory-loc">新大久保</p>
+                    <p class="memory-cap">辛いものいっぱいかと思ったけどそうではなさそう…？</p>
+                </div>
+                <div class="memory" style="top: 75px;">
+                    <div class="frame-normal"></div>
+                    <p class="memory-date">Jun.04, 2022</p>
+                    <p class="memory-loc">越谷レイクタウン</p>
+                    <p class="memory-cap">VSパークに遊びに行った〜</p>
+                </div>
+                <div class="memory" style="top: 90px;">
+                    <div class="frame-normal"></div>
+                    <p class="memory-date">Jun.04, 2022</p>
+                    <p class="memory-loc">越谷レイクタウン</p>
+                    <p class="memory-cap">卵と私に初潜入！美味しかった〜！</p>
+                </div>
+                <div class="memory" style="top: 105px;">
+                    <div class="frame-normal"></div>
+                    <p class="memory-date">May.30, 2022</p>
+                    <p class="memory-loc">南大沢</p>
+                    <p class="memory-cap">やることやって南大沢へ！</p>
+                </div>
             </div>
-            <div class="memory" style="top: 15px;">
-                <div class="frame-normal"></div>
-                <p class="memory-date">Jun.04, 2022</p>
-                <p class="memory-loc">越谷レイクタウン</p>
-                <p class="memory-cap">VSパークに遊びに行った〜</p>
-            </div>
-            <div class="memory" style="top: 30px;">
-                <div class="frame-normal"></div>
-                <p class="memory-date">Jun.04, 2022</p>
-                <p class="memory-loc">越谷レイクタウン</p>
-                <p class="memory-cap">卵と私に初潜入！美味しかった〜！</p>
-            </div>
-            <div class="memory" style="top: 45px;">
-                <div class="frame-normal"></div>
-                <p class="memory-date">May.30, 2022</p>
-                <p class="memory-loc">南大沢</p>
-                <p class="memory-cap">やることやって南大沢へ！</p>
-            </div>
-            <div class="memory" style="top: 60px;">
-                <div class="frame-normal"></div>
-                <p class="memory-date">Jun.10, 2022</p>
-                <p class="memory-loc">新大久保</p>
-                <p class="memory-cap">辛いものいっぱいかと思ったけどそうではなさそう…？</p>
-            </div>
-            <div class="memory" style="top: 75px;">
-                <div class="frame-normal"></div>
-                <p class="memory-date">Jun.04, 2022</p>
-                <p class="memory-loc">越谷レイクタウン</p>
-                <p class="memory-cap">VSパークに遊びに行った〜</p>
-            </div>
-            <div class="memory" style="top: 90px;">
-                <div class="frame-normal"></div>
-                <p class="memory-date">Jun.04, 2022</p>
-                <p class="memory-loc">越谷レイクタウン</p>
-                <p class="memory-cap">卵と私に初潜入！美味しかった〜！</p>
-            </div>
-            <div class="memory" style="top: 105px;">
-                <div class="frame-normal"></div>
-                <p class="memory-date">May.30, 2022</p>
-                <p class="memory-loc">南大沢</p>
-                <p class="memory-cap">やることやって南大沢へ！</p>
-            </div>
-        </div>
 
     
     
-        <!-- 作成 -->
-        <form action="<?php $thisfilename ;?>" method="POST">
-            <div class="create">
-                
-                <a href="create_diary.php"><input type="button" class="create_btn"></a>
-                <p class="create_txt">作成</p>
-                <p class="create_icon"><img src="../static/icon/create_icon.png"></p>
-                
-                <!-- <input type="submit" name="create"> -->
-                
-            </div>
+            <!-- 作成 -->
+            <form action="<?php $thisfilename ;?>" method="POST">
+                <div class="create">
+                    
+                    <a href="create_diary.php"><input type="button" class="create_btn"></a>
+                    <p class="create_txt">作成</p>
+                    <p class="create_icon"><img src="../static/icon/create_icon.png"></p>
+                    
+                    <!-- <input type="submit" name="create"> -->
+                    
+                </div>
 
-            
+                
 
-            <!-- 検索 -->
-            <div class="v39_173">
-                <!-- <div class="v39_170"></div> -->
-                <input type="submit" name="search">
-                <input type="text" class="v39_170" name="input_search">
-                <p class="v39_172">検索する</p>
-                
-                
-            </div>
-        </form>
-    </div>
-</div>
+                <!-- 検索 -->
+                <div class="v39_173">
+                    <!-- <div class="v39_170"></div> -->
+                    <input type="submit" name="search">
+                    <input type="text" class="v39_170" name="input_search">
+                    <p class="v39_172">検索する</p>
+                    
+                    
+                </div>
+            </form>
+        <!-- </div>
+    </div> -->
 
 
 <?php
