fc631ff478edc1a2fcd6154bba3ec9735ecc4dbf
Shimpo-Yumiko <s2022056@stu.musashino-u.ac.jp>
2022-08-02 10:18:01 +0900

    php card test

--- MyApp/views/home.php
+++ MyApp/views/home.php
@@ -69,6 +69,36 @@ if (!isset($_SESSION["user_id"])) {
             <div class="memorys">
 <?php
 
+// $diary_row = selectdata("diary",$user_id);
+// $num_px = 0;
+// for ($i = 0; $i < pg_num_rows($diary_row); $i++){
+//     $rows = pg_fetch_array($diary_row,NULL,PGSQL_ASSOC);
+//     $diary_id = $rows["diary_id"];
+
+//     echo "<form action='detail_diary.php' method='POST' name='show_detail'>";
+//     echo "<button type='submit' class='frame-select'>";
+//     echo '<div class="memory" style="top: ${num_px}px;">';
+//     echo '<div class="frame-select"></div>';
+//     echo '<p class="memory-date">'.$rows["register_at"].'</p>';
+//     echo '<p class="memory-loc">'.$rows["spot_name"].'</p>';
+//     echo '<p class="memory-cap">'.$rows["content"].'</p>';
+
+    
+//     echo "<input type='hidden' name='diary_id' value='${diary_id}'>";
+//     // echo "<input type='submit' value='詳細'>";
+//     echo '</div>';
+//     echo "</button>";
+
+//     echo "</form>";
+
+
+//     $num_px += 15;
+// }
+
+
+?>
+<?php
+
 $diary_row = selectdata("diary",$user_id);
 $num_px = 0;
 for ($i = 0; $i < pg_num_rows($diary_row); $i++){
@@ -77,16 +107,16 @@ for ($i = 0; $i < pg_num_rows($diary_row); $i++){
 
     echo "<form action='detail_diary.php' method='POST' name='show_detail'>";
     echo "<button type='submit' class='frame-select'>";
-    echo '<div class="memory" style="top: ${num_px}px;">';
-    echo '<div class="frame-select"></div>';
-    echo '<p class="memory-date">'.$rows["register_at"].'</p>';
-    echo '<p class="memory-loc">'.$rows["spot_name"].'</p>';
-    echo '<p class="memory-cap">'.$rows["content"].'</p>';
+    echo '<div class="card flex-md-row mb-4 shadow-sm h-md-250">';
+    echo '<div class="card-body d-flex flex-column align-items-start">';
+    echo '<h3 class="md-0"><p class="text-dark">'.$rows["register_at"].'</p></h3>';
+    echo '<p class="mb-1 text-muted">'.$rows["spot_name"].'</p>';
+    echo '<p class="mcard-text mb-auto">'.$rows["content"].'</p>';
 
     
     echo "<input type='hidden' name='diary_id' value='${diary_id}'>";
     // echo "<input type='submit' value='詳細'>";
-    echo '</div>';
+    echo '</div></div>';
     echo "</button>";
 
     echo "</form>";
