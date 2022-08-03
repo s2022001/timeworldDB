f8a387924c3c4e5b8af031c4385b8d75d31b1277
Shimpo-Yumiko <s2022056@stu.musashino-u.ac.jp>
2022-08-03 15:29:43 +0900

    move script

--- MyApp/views/maptest.php
+++ MyApp/views/maptest.php
@@ -2,30 +2,10 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Step6.マーカーをクリックしてポップアップ表示とツールチップ表示|Lefletの基本|埼玉大学谷謙二研究室</title>
+        <title>map</title>
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.0/dist/leaflet.css" />
         <script src="https://unpkg.com/leaflet@1.3.0/dist/leaflet.js"></script>
-        <script>
-            function map(lat, lon, date, spot) {
-                var map = L.map('mapcontainer', { zoomControl: false });
-                var loc = [lat, lon];
-                var date = date;
-                var spot = spot;
-                map.setView(loc, 15);
-                L.tileLayer('https://c.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    attribution: "<a href='https://www.openstreetmap.org/copyright' target='_blank'>OpenStreetMap</a> contributors, "
-                }).addTo(map);
-                //ポップアップする文字（HTML可、ここでは画像を表示）
-                var sucontents = date + "<br>" + spot;
-                //ポップアップオブジェクトを作成
-                var popup1 = L.popup({ maxWidth: 550 }).setContent(sucontents);
-                var date = "2022/07/31";
-                var popup2 = L.popup().setContent(date + "<br>" + spot);
-                //マーカーにポップアップを紐付けする。同時にbindTooltipでツールチップも追加
-                L.marker(loc).bindPopup(popup1).bindTooltip(spot).addTo(map);
-                L.marker([35.8561, 139.6098]).bindPopup(popup2).bindTooltip("桜区役所").addTo(map);
-            }
-        </script>
+        <script src="../static/js/myjs.js"></script>
     </head>
     
 <?php
